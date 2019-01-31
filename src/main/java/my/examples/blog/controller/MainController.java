a4c95fa11c205bf07887a50f9be621d250fcac31
EunHyunCho <ouoyy6395@gamil.com>
2019-01-31 11:58:40 +0900

    Initial commit

---
+++ src/main/java/my/examples/blog/controller/MainController.java
@@ -0,0 +1,14 @@
+package my.examples.blog.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+    @GetMapping("/main")
+    public String main(Model model){
+        model.addAttribute("name", "urstory");
+        return "index";
+    }
+}
